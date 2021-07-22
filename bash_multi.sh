fed8184d345ecbd40dfec1cde338f0f6999af728
yangsvm <852488062@qq.com>
2021-07-23 00:30:40 +0800

    add result

--- bash_multi.sh
+++ bash_multi.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 export PYTHONPATH=$PYTHONPATH:/home/tiecun/codes/MARL/simulation/smac
 
-for i in {0..2} ;
+
+for i in {7..9} ;
 do
-    ( python main.py --alg=task_decomposition --map=2s3z --multi_process_n=$i & )
+    ( python main.py --alg=qmix --map=3m --multi_process_n=$i & )
 done
