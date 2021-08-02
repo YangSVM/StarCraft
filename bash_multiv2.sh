d7cf4c61e979f1f6281f3e93c3171661d36c2dcb
yangSVM <852488062@qq.com>
2021-08-03 01:57:53 +0800

    upload bash

--- bash_multiv2.sh
+++ bash_multiv2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PYTHONPATH=$PYTHONPATH:/home/ubuntu/codes/marl/smac
 
-for i in {8..9} ;
+for i in {1..2} ;
 do
-    ( python main.py --alg=qmix --map=2s3z --n_tasks=5 --multi_process_n=$i & )
+    ( python main.py --alg=task_decomposition_all_without_task --map=2s3z --task_dec_type=n_enemy --multi_process_n=$i & )
 done
