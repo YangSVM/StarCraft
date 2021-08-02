43a9ac89af7bcd6a6b3ec716fc13bf376316dfe4
yangsvm <852488062@qq.com>
2021-08-03 01:56:23 +0800

    fix bugs

--- bash_multiv2.sh
+++ bash_multiv2.sh
@@ -3,5 +3,5 @@ export PYTHONPATH=$PYTHONPATH:/home/ubuntu/codes/marl/smac
 
 for i in {8..9} ;
 do
-    ( python main.py --alg=qmix --map=2s3z --n_tasks=5 --multi_process_n=$i & )
+    ( python main.py --alg=qmix --map=2s3z --task_dec_type=n_enemy --multi_process_n=$i & )
 done
